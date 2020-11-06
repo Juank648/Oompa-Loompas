2274aeb50b82614fc77961d0516f09042d9b466c
Jose Castro Ortega <jose.castro-o@mail.escuelaing.edu.co>
2020-11-05 20:20:32 -0500

    Se agrega JavaScript para animación

--- labinfo/src/main/webapp/js/main.js
+++ labinfo/src/main/webapp/js/main.js
@@ -1,99 +1,25 @@
-var listaUsuarios = JSON.parse(localStorage.getItem('listaDeUsuariosLs'));
-function obtenerListaDeUsuarios(){
-	
-
-	if(listaUsuarios == null  ){
-		listaUsuarios = [
-			['jose','jose.castro@gmail.com','jose123'],['andres','andres.molina@gmail.com',
-			'molina123']
-		]
-	}
-	/*if(aa!= null) {
-		listaUsuarios.push([a]);
-	}*/
-
-	return listaUsuarios;
-}
-
-function agregarUsuario(usuario, correo,contraseña){
-	console.log('Entra agregrar');
-	console.log(usuario);
-	console.log(contraseña);
-	console.log(correo);
-	var listaUsuarios = obtenerListaDeUsuarios();
-
-	listaUsuarios.push([usuario,correo,contraseña]);
-	console.log('Ya agregó');
-	console.log(listaUsuarios[2][0]);
-}
-
-function validarCredenciales(pCorreo, pContraseña){
-	var listaUsuarios = obtenerListaDeUsuarios();
-	var bAcceso = false;
-	for(var i=0; i<listaUsuarios.length; i++){
-		if(pCorreo == listaUsuarios[i][0] && pContraseña == listaUsuarios[i][2]){
-			bAcceso = true;
-
-		
-			
-		}
-
-	}
+function ocultar(){
 	
 	
-
-	return bAcceso;
-
-}
-
-
-
-$('.botonCuenta').click(function(){
-    $('.formulario').animate({
-        height: "toggle",
-        'padding-top': 'toggle',
-        'padding-bottom': 'toggle'
-    }, "slow");
-});
-
-
-document.querySelector('#boton').addEventListener('click',iniciarSesion);
-
-
-
-function iniciarSesion(){
-	var mensaje = 'hola';
-	var sCorreo = '';
-	var sContraseña = '';
-	var bAcceso = false;
-
-	sCorreo = document.querySelector('#atributo').value;
-	sContraseña = document.querySelector('#contra').value;
-
-	bAcceso = validarCredenciales(sCorreo,sContraseña);
+	document.getElementById("segundo").style.display="none";
+	document.getElementById("tercero").style.display="block";
+	document.getElementById("pr").style.display="none";
+	document.getElementById("sg").style.display="block";
 	
-	if(bAcceso==true){
-		alert('Bienvenido'+' '+sCorreo);
-		window.location.href = 'pagina.html';
-	}
-	else{
-		alert('Usuario o contraseña incorrecta')
-	}
 }
 
-
-function volver(){
-	console.log('Entra');
-	window.location.href = 'index.html';
+function mostrar(){
+	
+	
+	document.getElementById("segundo").style.display="block";
+	document.getElementById("tercero").style.display="none";
+	document.getElementById("sg").style.display="none";
+	document.getElementById("pr").style.display="block";
+	
 }
 
-document.querySelector('#botonRegistro').addEventListener('click',registrarse);
+//document.getElementById("pr").click()=ocultar();
 
 
-function registrarse() {
-	usuario = document.querySelector('#registoUsuario').value;
-	correo = document.querySelector('#correo').value;
-	contraseña = document.querySelector('#registoContra').value;
 
-	agregarUsuario(usuario,correo,contraseña);
-}
+
